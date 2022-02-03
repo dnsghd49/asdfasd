398eced60e18d56a385024ceda4f6b0ccb7f2e0d
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-02 21:23:02 -0500

    the version of react-router-dom is causing the problem atm, check point

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import Gallery from './components/Gallery'
 import Searchbar from './components/Searchbar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
 
-  const API_url = "https://itunes.apple.com/search?term="
   useEffect(() => {
-    const fetchData = async () => {
+    const API_URL = `https://itunes.apple.com/search?term=`
+    if (search) {
       document.title = `${search} Music`
-      const response = await fetch(API_url + search)
-      const resData = await response.json()
-      if (resData.results.length > 0) {
-        setData(resData.results)
-      } else {
-        setMessage('Not Found')
+      const fetchData = async () => {
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
       }
+      fetchData()
     }
-    fetchData()
   }, [search])
+
 
   const handleSearch = (e, term) => {
     e.preventDefault()
@@ -30,9 +36,22 @@ function App() {
 
   return (
     <div className='App'>
-      <Searchbar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <BrowserRouter>
+        
+        <Route exact path="/">
+          <Searchbar handleSearch={handleSearch} />
+          <Gallery data={data} />
+        </Route>
+
+        <Route exact path="/album/:id">
+          <AlbumView />
+        </Route>
+
+        <Route exact path="/artist/:id">
+          <ArtistView />
+        </Route>
+      </BrowserRouter>
     </div>
   )
 }
